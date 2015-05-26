f152348ee66ec8d2dfb06f023f1722649dd916e0
JZ <juanzox@gmail.com>
2015-05-26 09:30:45 -0300

    Stop words incorporated.

--- src/docgraph_example.cpp
+++ src/docgraph_example.cpp
@@ -12,8 +12,10 @@
 #include <list>
 #include <fstream>
 #include <sstream>
-
+#include <set>
 #include "docgraph.hpp"
+
+
 
 using namespace std;
 
@@ -21,36 +23,20 @@ using namespace std;
  * Extract all pairs between the pivot word and every word in the list Q.
  * Then inserts each pair into the graph G.
  */
-void extract_edges(const string &p, const list<string>& Q, DG::docgraph& G) {
+void extract_edges(const string &p, const list<string>& Q, DG::docgraph& G, const set<string>& stopwords) {
+
+	if(stopwords.find(p) != stopwords.cend() )
+		return;
+
 	for (auto s = Q.begin(); s != Q.end(); ++s) {
 		//cout << "edge: " << p << " -- " << *s << endl;
-		G.add_edge(p, *s);
+		if(stopwords.find(*s) == stopwords.cend() )
+			G.add_edge(p, *s);
 
 	}
 }
 
-/**
- * Reads data in cluto's format (sparse or non sparse autodetection).
- * Stores the data and their labels into a Realdata object.
- * */
-void read_csv_string_data(const char * fname) {
-	cout << "openning file " << fname << endl;
-	ifstream inf(fname, iostream::in);
-	char c;
-	int nlines = 0;
-	//while (!inf.eof()) {
-	while (inf.good()) {
-		inf.get(c);
-		if (c == '\n') {
-			cout << "EOL\n";
-			nlines++;
-		} else
-			cout << c << "|";
-	}
 
-	cout << "NLINES:" << nlines << endl;
-	inf.close();
-}
 
 enum FILESTATUS {
 	KEEP_READING, EOL_FOUND, EOF_FOUND
@@ -102,20 +88,43 @@ FILESTATUS next_token(ifstream &inf, string& s) {
 	return STAT; // returned true if there are more chars to read and false when an EOL o EOF are found.
 }
 
+void init_stopwords(const char * infile, set<string>& stopwords){
+	cout << "Reading stopwords from "<< infile << endl;
+	char name[256];
+	ifstream inf(infile, iostream::in);
+
+	while(inf.good() ){
+		inf.getline (name,256);
+		if( inf.gcount() > 0 )
+			stopwords.insert( name);
+	}
+	inf.close();
+}
 
 int main(int argc, char* argv[]) {
 
-	if (argc < 3) {
-		cout << "Enter the filename and windows-size.\n";
+	if (argc < 4) {
+		cout << "Enter the input filename, stopword filename and windows-size.\n";
 		return -1;
 	}
 
+	/* Open input file */
 	ifstream inf(argv[1], iostream::in);
 
+	/* Set window size and create empty document graph */
+	int wsize = stoi(argv[3]);
 	DG::docgraph dgraph;
-	int wsize = stoi(argv[2]);
-	cout << "Loading file "<<argv[1] << ". Using window-size "<<wsize << endl;
 
+
+	/* Load stopwords */
+	set<string> stopwords;
+	init_stopwords(argv[2], stopwords);
+
+
+
+	cout << "Loading input file "<<argv[1] << ". Using stopwords file "<< argv[2]<<" and window-size "<<wsize << endl;
+
+	/* Extract tokens and build biterm net */
 	list<string> queue;
 	string pivot, t;
 	FILESTATUS val = next_token(inf, t); //FILEID
@@ -126,11 +135,11 @@ int main(int argc, char* argv[]) {
 		val = next_token(inf, t);
 
 		if (val == EOL_FOUND) {
-			extract_edges(pivot, queue, dgraph);
+			extract_edges(pivot, queue, dgraph, stopwords);
 			while (queue.size() > 1) {
 				pivot = queue.front();
 				queue.pop_front();
-				extract_edges(pivot, queue, dgraph);
+				extract_edges(pivot, queue, dgraph, stopwords);
 			}
 			// end of current document
 			queue.clear();
@@ -139,7 +148,7 @@ int main(int argc, char* argv[]) {
 		} else if (queue.size() < (wsize - 1)) {
 			queue.push_back(t);
 		} else { // |queue| == (w - 1)
-			extract_edges(pivot, queue, dgraph);
+			extract_edges(pivot, queue, dgraph, stopwords);
 			pivot = queue.front();
 			queue.pop_front();
 			queue.push_back(t);
@@ -147,89 +156,9 @@ int main(int argc, char* argv[]) {
 	}
 
 	inf.close();
+
+	/* Dump resulting graph */
 	//dgraph.traverse_edges();
 
 	dgraph.graphml_dump("test.graphml");
-}
-
-int main2(int argc, char* argv[]) {
-	vector<string> ts;
-	ts.push_back("19828283");
-	ts.push_back("este");
-	ts.push_back("mensaje");
-	ts.push_back("de");
-	ts.push_back("este");
-	ts.push_back("tema");
-	ts.push_back("no");
-	ts.push_back("habla");
-	ts.push_back("de");
-	ts.push_back("ningun");
-	ts.push_back("mensaje");
-
-	DG::docgraph dgraph;
-
-	list<string> queue;
-	string pivot, t;
-
-	int wsize = 3;
-	if (argc > 1) {
-		wsize = stoi(argv[1]);
-		cout << "wsize set to " << wsize << endl;
-	}
-
-	/* the algorithm begins
-	 * It assumes that the first field represents the FILEID, so it's not considered.
-	 * */
-
-	auto it = ts.begin();
-	cout << "FILEID: " << *it << endl;
-
-	pivot = *(++it); // |queue| = 0
-	while (1) {
-		t = *(++it);
-		if (it == ts.end()) {
-			extract_edges(pivot, queue, dgraph);
-			while (queue.size() > 1) {
-				pivot = queue.front();
-				queue.pop_front();
-				extract_edges(pivot, queue, dgraph);
-			}
-			break; // end of iteration
-		} else if (queue.size() < (wsize - 1)) {
-			queue.push_back(t);
-		} else { // |queue| == (w - 1)
-			extract_edges(pivot, queue, dgraph);
-			pivot = queue.front();
-			queue.pop_front();
-			queue.push_back(t);
-		}
-	}
-
-//cout << "Final queue size:" << queue.size() << endl;
-
-	dgraph.traverse_edges();
-
-	return 0;
-}
-
-int main3(int argc, char* argv[]) {
-
-	if (argc == 1) {
-		cout << "Enter the filename\n";
-		return -1;
-	}
-
-	ifstream inf(argv[1], iostream::in);
-	string t;
-	FILESTATUS val = next_token(inf, t);
-	while (val != EOF_FOUND) {
-		if (val == EOL_FOUND) {
-			cout << "\n";
-		} else {
-			cout << t << ":";
-		}
-		val = next_token(inf, t);
-	}
-	inf.close();
-	return 0;
 }
